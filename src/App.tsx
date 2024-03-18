d9cb27a23346e4e215d368307176a5631c30d89e
Umer Nasir <umernasirr@gmail.com>
2024-03-18 16:42:18 +0500

    cleaning up code

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,6 @@ function App() {
       <Toaster />
       <CookiesProvider>
         <IdProvider>
-          {/* <Toaster /> */}
           <RouterProvider router={router} />
         </IdProvider>
       </CookiesProvider>
